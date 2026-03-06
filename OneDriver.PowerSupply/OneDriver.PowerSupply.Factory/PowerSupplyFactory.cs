d7907304e2faf36615b90c48b3390755473a49dd
Rahul <83376754+himalayanpeaks@users.noreply.github.com>
2026-03-07 00:49:49 +0100

    Factory returns null for unknown device tpye

--- OneDriver.PowerSupply/OneDriver.PowerSupply.Factory/PowerSupplyFactory.cs
+++ OneDriver.PowerSupply/OneDriver.PowerSupply.Factory/PowerSupplyFactory.cs
@@ -10,24 +10,9 @@ namespace OneDriver.PowerSupply.Factory
         Virtual,
         Kd3005p
     }
-    /*public class AbstractPowerSupply
-    {
-        public CommonDeviceParams Parameters { get;  }
-
-        public AbstractPowerSupply(CommonDeviceParams parameters, IPowerSupplyFunctions methods, ObservableCollection<BaseChannelWithProcessData<CommonChannelParams, CommonProcessData>> elements)
-        {
-            Parameters = parameters;
-            Methods = methods;
-            Elements = elements;
-        }
-
-        public IPowerSupplyFunctions Methods { get;  }
-        public ObservableCollection<CommonChannel<CommonChannelParams, CommonProcessData>> Elements { get; }
-    }*/
-
     public class PowerSupplyFactory
     {
-        public static CommonDevice<CommonDeviceParams, CommonChannelParams, CommonProcessData> Create(PowerSupplyType deviceType)
+        public static CommonDevice<CommonDeviceParams, CommonChannelParams, CommonProcessData>? Create(PowerSupplyType deviceType)
         {
             switch (deviceType)
             {
